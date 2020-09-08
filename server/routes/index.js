1965941203916a1826ea376d55ed846512eedef8
Paterson Anaccius <Paterson@23people.io>
2020-09-08 14:04:16 -0300

    remove unnecessary env var

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 const postsRoutes = require('./posts.routes');
 
-const { BASE_API_URL } = process.env;
-
 function Router(server) {
-    server.use(`${BASE_API_URL}/posts`, postsRoutes);
+    server.use(`api/posts`, postsRoutes);
 }
 
 module.exports = Router;
